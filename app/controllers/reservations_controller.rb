4e7a98b6706d3a1aca085f351cbf6bab7d82165f
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-23 19:06:46 -0400

    Reservation and sign up are now working

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,11 +4,29 @@ class ReservationsController < ApplicationController
   end
 
   def create
+  	@restaurant = load_rest
+  	@reservation = @restaurant.reservations.build(res_params)
+  	@reservation.user = current_user
+
+  	if @reservation.save
+  		redirect_to root_path, notice: "Reservation created successfully"
+  	else
+  		render 'restaurants/show'
+  	end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+  def res_params
+  	params.require(:reservation).permit(:name, :party_size, :booking_time)
+  end
+
+  def load_rest
+  	@restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
